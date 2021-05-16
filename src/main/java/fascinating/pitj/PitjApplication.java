821f0b8e4a039d5cdb38c606a5e56eef7cdee4de
nmin11 <75058239+nmin11@users.noreply.github.com>
2021-05-17 00:58:24 +0900

    이미지 출력 안되는 문제 해결 이미지 리사이징 요망

--- src/main/java/fascinating/pitj/PitjApplication.java
+++ src/main/java/fascinating/pitj/PitjApplication.java
@@ -2,6 +2,7 @@ package fascinating.pitj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PitjApplication {
