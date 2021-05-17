e3e124dc47ae1476ed750f0496521d04d529a5a6
nmin11 <75058239+nmin11@users.noreply.github.com>
2021-05-17 16:21:25 +0900

    join form 양식 작성

---
+++ src/main/resources/static/js/joinForm.js
@@ -0,0 +1,16 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("movement");
+
+
+function loginForm(){
+    x.style.left = "0px";
+    y.style.left = "400px";
+    z.style.left = "0";
+}
+
+function registerForm(){
+    x.style.left = "-350px";
+    y.style.left = "0px";
+    z.style.left = "120px";
+}
