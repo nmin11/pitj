c1c6dc5107a02d1555f6cd2cb59b4f1e32d2a767
nmin11 <75058239+nmin11@users.noreply.github.com>
2021-05-18 14:03:02 +0900

    join form html/css/js 작업

--- src/main/resources/static/js/joinForm.js
+++ src/main/resources/static/js/joinForm.js
@@ -2,7 +2,6 @@ var x = document.getElementById("login");
 var y = document.getElementById("register");
 var z = document.getElementById("movement");
 
-
 function loginForm(){
     x.style.left = "0px";
     y.style.left = "400px";
@@ -13,4 +12,17 @@ function registerForm(){
     x.style.left = "-350px";
     y.style.left = "0px";
     z.style.left = "120px";
+}
+
+function count_check(obj){
+    var theme = document.getElementsByName("register_theme");
+    var checkingCount = 0;
+    for(var i=0; i<theme.length; i++){
+        if(theme[i].checked){checkingCount++;}
+    }
+    if(checkingCount>2){
+        alert("2개까지만 체크 가능합니다!")
+        obj.checked = false;
+        return false;
+    }
 }
